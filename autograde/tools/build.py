c8fa14a981068e3c2109ffde2811e9dedaa8ae7d
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2020-02-28 23:08:32 -0600

    Minor changes

--- autograde/tools/build.py
+++ autograde/tools/build.py
@@ -1,41 +1,41 @@
 """Module that contains functions for building and/or compiling programs."""
 
+import json
+import shutil
 import subprocess
 from pathlib import Path
 from os import PathLike
+import autograde
 from autograde.components.program import Program
 from autograde.components.cpp_components import CppProgram
 from autograde.tools.result import CompileResult, Result
-from typing import Optional
+from typing import Optional, Tuple
 
 
-def create_scons(program: Program, target_dir: PathLike) -> Path:
+def create_scons(program: Program, target_dir: PathLike) -> Tuple[Path, Path]:
     """Returns a path a newly created scons file for a target program.
 
     args:
         program: A program to create a scons file for.
     """
+    sconstruct_template = Path(autograde.__file__, "templates", "SConstruct")
     target_dir = Path(target_dir)
-    construct_path = target_dir / 'SConstruct'
-    with construct_path.open('wt') as construct:
-        other_sources = program.source_files - {program.entry_point}
-        dependencies = ", ".join(
-            f"r'{sf.path.resolve()}'" for sf in other_sources
-        )
-        construct.write("\n".join(
-            "Object("
-            f"target = r'{target_dir / sf.path.name}',"
-            f"source = r'{sf.path.resolve()}')"
-            for sf in other_sources
-        ))
-        if program.entry_point is not None:
-            absolute_path = program.entry_point.path.resolve()
-            executable_path = target_dir / absolute_path.name
-            construct.write(
-                f"Program(r'{executable_path.with_suffix('.exe')}',"
-                f"[r'{str(absolute_path)}', {dependencies}])"
-            )
-    return construct_path
+    build_info_file = target_dir / 'build_info.json'
+    other_sources = program.source_files - {program.entry_point}
+    dependencies = ", ".join(str(sf.path.resolve()) for sf in other_sources)
+    build_info = {
+        "source_files": dependencies,
+        "executable_path": None
+    }
+    if program.entry_point is not None:
+        absolute_path = program.entry_point.path.resolve()
+        executable_path = target_dir / absolute_path.name
+        build_info["entry_point"] = str(absolute_path)
+        build_info["executable_path"] = str(executable_path.with_suffix('.exe'))
+    with build_info_file.open("wt") as bf:
+        json.dump(build_info, bf)
+    shutil.copyfile(sconstruct_template, target_dir / sconstruct_template.name)
+    return sconstruct_template, build_info_file
 
 
 def compile_cpp(
@@ -60,7 +60,7 @@ def compile_cpp(
     if program.entry_point is not None:
         executable = target_path / program.entry_point.path.name
         executable = executable.with_suffix(".exe")
-    create_scons(program, target_path)
+    scons_path, info_file = create_scons(program, target_path)
     proc_status = subprocess.run(
         ['scons'], shell=True, cwd=target_path, capture_output=True, text=True
     )
