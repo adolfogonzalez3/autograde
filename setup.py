1b6f93c702cf647d532e6c72faaa2c37285b1f4f
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2020-02-28 23:49:02 -0600

    Minor changes

--- setup.py
+++ setup.py
@@ -7,21 +7,23 @@ if sys.version_info.major != 3:
           'Python {}. The installation will likely fail.'.format(sys.version_info.major))
 
 
-setup(name='autograde',
-      packages=[package for package in find_packages()
-                if package.startswith('autograde')],
-      install_requires=[
-          'scons'
-      ],
-      extras_require={
-        
-      },
-      description='A library whose purpose is to help with grading.',
-      author='Adolfo Gonzalez III',
-      url='',
-      author_email='',
-      keywords="",
-      license="",
-      )
+setup(
+    name='autograde',
+    packages=[package for package in find_packages()
+              if package.startswith('autograde')],
+    install_requires=[
+        'scons'
+    ],
+    extras_require={
 
-
+    },
+    description='A library whose purpose is to help with grading.',
+    author='Adolfo Gonzalez III',
+    url='',
+    author_email='',
+    keywords="",
+    license="",
+    package_data={
+        "templates": ["SConstruct"]
+    }
+)
